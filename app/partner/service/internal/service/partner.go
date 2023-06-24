c4251e1007d377b2204fbbc32aae1c5ab9189284
lsy <945212191@qq.com>
2023-06-24 12:40:41 +0800

    feat: 增加登录状态校验与队伍入队逻辑

--- app/partner/service/internal/service/partner.go
+++ app/partner/service/internal/service/partner.go
@@ -9,27 +9,43 @@ import (
 )
 
 func (s *PartnerService) AddTeam(ctx context.Context, req *v1.Team) (*emptypb.Empty, error) {
-	team := &biz.CreateTeam{}
-	util.StructAssign(team, req)
-	err := s.vc.ParamsValidate(team)
+	//是否登录，未登录不允许创建
+	user, err := s.uc.IsUserLogin(ctx)
 	if err != nil {
 		return nil, err
 	}
-	err = s.pc.AddTeam(ctx, team)
+
+	team := &biz.CreateTeam{}
+	util.StructAssign(team, req)
+
+	team.UserId = user.Id
+	err = s.vc.ParamsValidate(team)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.pc.AddTeam(ctx, user, team)
 	if err != nil {
 		return nil, err
 	}
 	return &emptypb.Empty{}, nil
 }
 
-func (s *PartnerService) DeleteTeam(ctx context.Context, req *v1.Team) (*emptypb.Empty, error) {
-	team := &biz.DeleteTeam{
-		Id: req.Id,
-	}
-	err := s.vc.ParamsValidate(team)
+func (s *PartnerService) DeleteTeam(ctx context.Context, req *v1.DeleteTeamReq) (*emptypb.Empty, error) {
+	//是否登录，未登录不允许创建
+	_, err := s.uc.IsUserLogin(ctx)
 	if err != nil {
 		return nil, err
 	}
+
+	team := &biz.DeleteTeam{
+		Id: req.Id,
+	}
+	err = s.vc.ParamsValidate(team)
+	if err != nil {
+		return nil, err
+	}
+
 	err = s.pc.DeleteTeam(ctx, team)
 	if err != nil {
 		return nil, err
@@ -37,22 +53,36 @@ func (s *PartnerService) DeleteTeam(ctx context.Context, req *v1.Team) (*emptypb
 	return &emptypb.Empty{}, nil
 }
 
-func (s *PartnerService) UpdateTeam(ctx context.Context, req *v1.Team) (*emptypb.Empty, error) {
-	team := &biz.UpdateTeam{
-		Id: req.Id,
-	}
-	err := s.vc.ParamsValidate(team)
+func (s *PartnerService) UpdateTeam(ctx context.Context, req *v1.UpdateTeamReq) (*emptypb.Empty, error) {
+	//是否登录，未登录不允许修改
+	user, err := s.uc.IsUserLogin(ctx)
 	if err != nil {
 		return nil, err
 	}
-	err = s.pc.UpdateTeam(ctx, team)
+
+	//是否是管理员
+	isAdmin := true
+	err = s.uc.IsAdmin(ctx)
+	if err != nil {
+		isAdmin = false
+	}
+
+	team := &biz.UpdateTeam{}
+	util.StructAssign(team, req)
+
+	err = s.vc.ParamsValidate(team)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.pc.UpdateTeam(ctx, team, user.Id, isAdmin)
 	if err != nil {
 		return nil, err
 	}
 	return &emptypb.Empty{}, nil
 }
 
-func (s *PartnerService) GetTeam(ctx context.Context, req *v1.Team) (*v1.GetTeamResponse, error) {
+func (s *PartnerService) GetTeam(ctx context.Context, req *v1.GetTeamReq) (*v1.GetTeamResponse, error) {
 	team := &biz.GetTeam{
 		Id: req.Id,
 	}
@@ -60,6 +90,7 @@ func (s *PartnerService) GetTeam(ctx context.Context, req *v1.Team) (*v1.GetTeam
 	if err != nil {
 		return nil, err
 	}
+
 	result, err := s.pc.GetTeam(ctx, team)
 	if err != nil {
 		return nil, err
@@ -79,8 +110,16 @@ func (s *PartnerService) GetTeam(ctx context.Context, req *v1.Team) (*v1.GetTeam
 }
 
 func (s *PartnerService) GetTeamList(ctx context.Context, req *v1.GetTeamListReq) (*v1.GetTeamListResponse, error) {
+	//只有管理员才能查看加密还有非公开的房间
+	if req.Query.Status != 0 {
+		err := s.uc.IsAdmin(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
 	team := &biz.TeamQuery{}
 	util.StructAssign(team, req.Query)
+
 	teamList, err := s.pc.GetTeamList(ctx, team, req.Page, req.PageSize)
 	if err != nil {
 		return nil, err
@@ -88,16 +127,53 @@ func (s *PartnerService) GetTeamList(ctx context.Context, req *v1.GetTeamListReq
 
 	reply := &v1.GetTeamListResponse{}
 	for _, item := range teamList {
-		reply.Data = append(reply.Data, &v1.Team{
-			Id:          item.Id,
-			Name:        item.Name,
-			MaxNum:      item.MaxNum,
-			ExpireTime:  item.ExpireTime,
-			UserId:      item.UserId,
-			Status:      item.Status,
-			Password:    item.Password,
-			Description: item.Description,
+		reply.Data = append(reply.Data, &v1.GetTeamListResponse_TeamInfo{
+			Team: &v1.Team{
+				Id:          item.Id,
+				Name:        item.Name,
+				MaxNum:      item.MaxNum,
+				ExpireTime:  item.ExpireTime,
+				UserId:      item.UserId,
+				Status:      item.Status,
+				Description: item.Description,
+			},
+			UserInfo: &v1.User{
+				Id:          item.UserInfo.Id,
+				UserName:    item.UserInfo.UserName,
+				UserAccount: item.UserInfo.UserAccount,
+				AvatarUrl:   item.UserInfo.AvatarUrl,
+				Phone:       item.UserInfo.Phone,
+				Email:       item.UserInfo.Email,
+				UserStatus:  item.UserInfo.UserStatus,
+				Gender:      item.UserInfo.Gender,
+				UserRole:    item.UserInfo.Role,
+				CreateTime:  item.UserInfo.CreateTime.String(),
+				Tags:        item.UserInfo.Tags,
+				Profile:     item.UserInfo.Profile,
+			},
 		})
 	}
 	return reply, nil
+}
+
+func (s *PartnerService) JoinTeam(ctx context.Context, req *v1.JoinTeamReq) (*emptypb.Empty, error) {
+	//是否登录，未登录不允许修改
+	//user, err := s.uc.IsUserLogin(ctx)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	joinTeam := &biz.JoinTeam{}
+	util.StructAssign(joinTeam, req)
+	err := s.vc.ParamsValidate(joinTeam)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.pc.JoinTeam(ctx, joinTeam, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
 }
