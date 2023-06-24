296e0098d72839f714d03459c0a08198d714447e
lsy <945212191@qq.com>
2023-06-24 12:47:03 +0800

    feat: error 信息整合 + 增加用户是否登录的外调函数

--- app/partner/service/internal/biz/user.go
+++ app/partner/service/internal/biz/user.go
@@ -92,14 +92,14 @@ func (i *User) DoValidate(trans ut.Translator, validate *validator.Validate) err
 //1. 判断是否有管理员权限
 //2. 根据用户名进行模糊查询
 func (r *UserUseCase) SearchUsers(ctx context.Context, userName string) ([]*User, error) {
-	err := r.isAdmin(ctx)
+	err := r.IsAdmin(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	users, err := r.repo.SearchUsers(ctx, userName)
 	if err != nil {
-		return nil, v1.ErrorUserSearchFailed("%s", err.Error())
+		return nil, v1.ErrorUserSearchFailed("用户搜索失败: %s", err.Error())
 	}
 	return users, nil
 }
@@ -107,11 +107,11 @@ func (r *UserUseCase) SearchUsers(ctx context.Context, userName string) ([]*User
 // SearchUsersByTags 根据标签搜索用户
 func (r *UserUseCase) SearchUsersByTags(ctx context.Context, tagList []string) ([]*User, error) {
 	if len(tagList) == 0 {
-		return nil, v1.ErrorUserTagsEmpty("user search tags is empty")
+		return nil, v1.ErrorUserSearchFailed("搜索标签不能为空")
 	}
 	users, err := r.repo.SearchUsersByTags(ctx, tagList)
 	if err != nil {
-		return nil, v1.ErrorUserSearchFailed("%s", err.Error())
+		return nil, v1.ErrorUserSearchFailed("用户搜索失败: %s", err.Error())
 	}
 	return users, nil
 }
@@ -121,7 +121,7 @@ func (r *UserUseCase) SearchUsersByTagsInCache(ctx context.Context, tagList []st
 	// 先把所有用户从数据库中取出
 	users, err := r.repo.SearchUsers(ctx, "")
 	if err != nil {
-		return nil, v1.ErrorUserSearchFailed("%s", err.Error())
+		return nil, v1.ErrorUserSearchFailed("用户搜索失败: %s", err.Error())
 	}
 
 	result := make([]*User, 0)
@@ -135,7 +135,7 @@ OuterLoop:
 		tagsMap := make(map[string]int, 0)
 		err = json.Unmarshal([]byte(user.Tags), &tagsMap)
 		if err != nil {
-			return nil, v1.ErrorUnknownError("%s", err.Error())
+			return nil, v1.ErrorUnknownError("未知错误: %s", err.Error())
 		}
 		for _, tag := range tagList {
 			// 若有一个标签不存在，则下一个
@@ -153,14 +153,14 @@ OuterLoop:
 //1. 判断是否有管理员权限
 //2. 根据用户id进行逻辑删除
 func (r *UserUseCase) DeleteUser(ctx context.Context, userId int32) error {
-	err := r.isAdmin(ctx)
+	err := r.IsAdmin(ctx)
 	if err != nil {
 		return err
 	}
 
 	err = r.repo.DeleteUser(ctx, userId)
 	if err != nil {
-		return v1.ErrorUserDeleteFailed("%s", err.Error())
+		return v1.ErrorUserDeleteFailed("用户删除失败: %s", err.Error())
 	}
 	return nil
 }
@@ -178,7 +178,7 @@ func (r *UserUseCase) GetCurrentUser(ctx context.Context) (*User, bool, error) {
 	}
 	user, err = r.repo.GetCurrentUser(ctx, user.Id)
 	if err != nil {
-		return nil, false, v1.ErrorUnknownError("%s", err.Error())
+		return nil, false, v1.ErrorUnknownError("未知错误: %s", err.Error())
 	}
 	return user, false, nil
 }
@@ -191,18 +191,18 @@ func (r *UserUseCase) UpdateUser(ctx context.Context, updateUser *UpdateUser) er
 		return err
 	}
 	if !exist {
-		return v1.ErrorLoginStateTimeout("")
+		return v1.ErrorLoginStateTimeout("登录已过期，请重新登录")
 	}
 
 	// 既不是管理员，也不是修改自己的，直接退出
-	if err = r.isAdmin(ctx); err != nil && user.Id != updateUser.Id {
-		return v1.ErrorPermissionDeny("update user: %v", updateUser)
+	if err = r.IsAdmin(ctx); err != nil && user.Id != updateUser.Id {
+		return v1.ErrorPermissionDeny("没有权限更新: %v", updateUser)
 	}
 
 	updateUser.Id = user.Id
 	err = r.repo.UpdateUser(ctx, updateUser)
 	if err != nil {
-		return v1.ErrorUpdateUser("%s", err.Error())
+		return v1.ErrorUpdateUser("用户信息修改错误: %s", err.Error())
 	}
 	return nil
 }
@@ -211,23 +211,35 @@ func (r *UserUseCase) UpdateUser(ctx context.Context, updateUser *UpdateUser) er
 func (r *UserUseCase) UsersRecommend(ctx context.Context, pageNum, pageSize int32) ([]*User, error) {
 	usersList, err := r.repo.GetUsersList(ctx, pageNum, pageSize)
 	if err != nil {
-		return nil, v1.ErrorUsersRecommendFailed("%s", err.Error())
+		return nil, v1.ErrorUsersRecommendFailed("用户推荐错误: %s", err.Error())
 	}
 	return usersList, nil
 }
 
-func (r *UserUseCase) isAdmin(ctx context.Context) error {
+// IsUserLogin 用户登录判断
+func (r *UserUseCase) IsUserLogin(ctx context.Context) (*User, error) {
+	user, exist, err := r.isSessionExist(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if !exist {
+		return nil, v1.ErrorUserLoginFailed("用户登录失败或账号不存在")
+	}
+	return user, nil
+}
+
+func (r *UserUseCase) IsAdmin(ctx context.Context) error {
 	userId, role, err := r.repo.GetUserRole(ctx)
 	if kerrors.IsNotFound(err) {
-		return v1.ErrorLoginStateTimeout("")
+		return v1.ErrorLoginStateTimeout("登录已过期，请重新登录")
 	}
 
 	if err != nil {
-		return v1.ErrorUnknownError("%s", err.Error())
+		return v1.ErrorUnknownError("未知错误: %s", err.Error())
 	}
 
 	if role != r.conf.AdminRole {
-		return v1.ErrorPermissionDeny("userId: %s, userRole: %v", userId, role)
+		return v1.ErrorPermissionDeny("没有权限: userId: %s, userRole: %v", userId, role)
 	}
 	return nil
 }
@@ -238,7 +250,7 @@ func (r *UserUseCase) isSessionExist(ctx context.Context) (*User, bool, error) {
 		return nil, false, nil
 	}
 	if err != nil {
-		return nil, false, v1.ErrorUnknownError("%s", err.Error())
+		return nil, false, v1.ErrorUnknownError("未知错误: %s", err.Error())
 	}
 	return user, true, nil
 }
